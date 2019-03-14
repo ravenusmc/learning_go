fe734e6b3458730c7321476f9e08fea304de494a
ravenusmc <mcuddy77@gmail.com>
2019-03-14 14:46:14 -0400

    finished challenge one

--- challenges/one.go
+++ challenges/one.go
@@ -1,2 +1,16 @@
 //Create a program that prompts for your name and than spits out a greeting
-//with your name 
+//with your name
+
+package main
+import (
+  "fmt"
+)
+
+func main() {
+  var name string
+  fmt.Println("What is your name?")
+  fmt.Scan(&name)
+
+  fmt.Println("Good day", name)
+
+}

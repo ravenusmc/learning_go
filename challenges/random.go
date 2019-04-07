134f03e8301f2053201f62ec2ffd52b03061ea63
ravenusmc <mcuddy77@gmail.com>
2019-04-07 10:42:58 -0400

    added function allow user to guess number

--- challenges/random.go
+++ challenges/random.go
@@ -9,11 +9,21 @@ import (
 
 func main() {
   number := getRandomNumber()
-  // fmt.Println(number)
+  makeGuess(number)
 }
 
+//This function will get a random number
 func getRandomNumber() int {
   rand.Seed(time.Now().UnixNano())
   number := rand.Intn(30)
   return number
+}
+
+//This function will allow the user to make a guess
+func makeGuess(number int) {
+  var guess string
+  fmt.Println("Please guess a number between 1 and 30:")
+  fmt.Scan(&guess)
+  fmt.Println("You guess is:", guess)
+  fmt.Println(number)
 }

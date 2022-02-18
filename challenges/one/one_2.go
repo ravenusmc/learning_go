e332ba1d5444bcd72b50648edc8279c45c99b884
ravenusmc <mcuddy77@gmail.com>
2022-02-18 14:30:32 -0500

    solved first problem

--- challenges/one/one_2.go
+++ challenges/one/one_2.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
+
+	var firstName string
+	var lastName string
 	
+  fmt.Println("What is your first name?")
+	fmt.Scan(&firstName)
+	
+	fmt.Println("What is your last name?")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Hello ", firstName, lastName)
 }
